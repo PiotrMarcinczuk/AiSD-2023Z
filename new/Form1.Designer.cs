d3d4fc2efbf95fa9a4e87d3f79ba0cd9e548c175
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-11-08 14:37:28 +0100

    AiSD sortowania v4

---
+++ new/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace @new
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Czas = new System.Windows.Forms.Label();
+            this.timeValue = new System.Windows.Forms.Label();
+            this.Generuj = new System.Windows.Forms.Button();
+            this.SB = new System.Windows.Forms.Button();
+            this.SS = new System.Windows.Forms.Button();
+            this.SI = new System.Windows.Forms.Button();
+            this.SM = new System.Windows.Forms.Button();
+            this.SQ = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 65);
+            this.textBox1.MaxLength = 900000000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 244);
+            this.textBox2.MaxLength = 900000000;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(379, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(587, 64);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Czas
+            // 
+            this.Czas.AutoSize = true;
+            this.Czas.Location = new System.Drawing.Point(524, 251);
+            this.Czas.Name = "Czas";
+            this.Czas.Size = new System.Drawing.Size(30, 13);
+            this.Czas.TabIndex = 5;
+            this.Czas.Text = "Czas";
+            this.Czas.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timeValue
+            // 
+            this.timeValue.AutoSize = true;
+            this.timeValue.Location = new System.Drawing.Point(587, 251);
+            this.timeValue.Name = "timeValue";
+            this.timeValue.Size = new System.Drawing.Size(0, 13);
+            this.timeValue.TabIndex = 6;
+            this.timeValue.Click += new System.EventHandler(this.timeValue_Click);
+            // 
+            // Generuj
+            // 
+            this.Generuj.Location = new System.Drawing.Point(590, 126);
+            this.Generuj.Name = "Generuj";
+            this.Generuj.Size = new System.Drawing.Size(75, 23);
+            this.Generuj.TabIndex = 7;
+            this.Generuj.Text = "Generuj";
+            this.Generuj.UseVisualStyleBackColor = true;
+            this.Generuj.Click += new System.EventHandler(this.Generuj_Click);
+            // 
+            // SB
+            // 
+            this.SB.Location = new System.Drawing.Point(87, 363);
+            this.SB.Name = "SB";
+            this.SB.Size = new System.Drawing.Size(75, 23);
+            this.SB.TabIndex = 8;
+            this.SB.Text = "SB";
+            this.SB.UseVisualStyleBackColor = true;
+            this.SB.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SS
+            // 
+            this.SS.Location = new System.Drawing.Point(226, 363);
+            this.SS.Name = "SS";
+            this.SS.Size = new System.Drawing.Size(75, 23);
+            this.SS.TabIndex = 9;
+            this.SS.Text = "SS";
+            this.SS.UseVisualStyleBackColor = true;
+            this.SS.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // SI
+            // 
+            this.SI.Location = new System.Drawing.Point(355, 363);
+            this.SI.Name = "SI";
+            this.SI.Size = new System.Drawing.Size(75, 23);
+            this.SI.TabIndex = 10;
+            this.SI.Text = "SI";
+            this.SI.UseVisualStyleBackColor = true;
+            this.SI.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // SM
+            // 
+            this.SM.Location = new System.Drawing.Point(479, 363);
+            this.SM.Name = "SM";
+            this.SM.Size = new System.Drawing.Size(75, 23);
+            this.SM.TabIndex = 11;
+            this.SM.Text = "SM";
+            this.SM.UseVisualStyleBackColor = true;
+            this.SM.Click += new System.EventHandler(this.SM_Click);
+            // 
+            // SQ
+            // 
+            this.SQ.Location = new System.Drawing.Point(606, 363);
+            this.SQ.Name = "SQ";
+            this.SQ.Size = new System.Drawing.Size(75, 23);
+            this.SQ.TabIndex = 12;
+            this.SQ.Text = "SQ";
+            this.SQ.UseVisualStyleBackColor = true;
+            this.SQ.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SQ);
+            this.Controls.Add(this.SM);
+            this.Controls.Add(this.SI);
+            this.Controls.Add(this.SS);
+            this.Controls.Add(this.SB);
+            this.Controls.Add(this.Generuj);
+            this.Controls.Add(this.timeValue);
+            this.Controls.Add(this.Czas);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label Czas;
+        private System.Windows.Forms.Label timeValue;
+        private System.Windows.Forms.Button Generuj;
+        private System.Windows.Forms.Button SB;
+        private System.Windows.Forms.Button SS;
+        private System.Windows.Forms.Button SI;
+        private System.Windows.Forms.Button SM;
+        private System.Windows.Forms.Button SQ;
+    }
+}
+
---
+++ new/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace @new
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Enabled = false;
+            Generuj.Enabled = false;
+        }
+
+        int[] Convert(string data)
+        {
+            try
+            {
+                string inputText = textBox1.Text;
+                var stringNumbers = inputText.Trim().Split(' ');
+
+                var numbers = new int[stringNumbers.Length];
+                for (int i = 0; i < stringNumbers.Length; i++)
+                {
+                    numbers[i] = int.Parse(stringNumbers[i]);
+                }
+                return numbers;
+            }
+            catch
+            {
+                MessageBox.Show("Zły format danych");
+                textBox1.Text = null;
+                textBox2.Text = null;
+                return null;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //checkbox
+            if (checkBox1.Checked)
+            {
+                textBox1.Enabled = false;
+                numericUpDown1.Enabled = true;
+                Generuj.Enabled = true;
+
+            }
+            else
+            {
+                textBox1.Enabled = true;
+                numericUpDown1.Enabled = false;
+                Generuj.Enabled = false;
+            }
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SB
+            int[] numbersTab = Convert(textBox1.Text);
+            if(numbersTab != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+
+                for (int i = 0; i < numbersTab.Length; i++)
+                {
+                    for (int j = 0; j < numbersTab.Length - 1; j++)
+                    {
+                        if (numbersTab[j] > numbersTab[j + 1])
+                        {
+                            int temp = numbersTab[j];
+                            numbersTab[j] = numbersTab[j + 1];
+                            numbersTab[j + 1] = temp;
+                        }
+                    }
+                }
+
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                timeValue.Text = timeSpan.TotalSeconds + " s";
+
+                string sortedTab = string.Join(" ", numbersTab);
+                textBox2.Text = sortedTab;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //SS
+            int[] numbersTab = Convert(textBox1.Text);
+
+            if(numbersTab != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int minIndex = 0;
+                int index = 0;
+                int temp = 0;
+                for (int i = 0; i < numbersTab.Length; i++)
+                {
+                    minIndex = i;
+                    for (int j = i + 1; j < numbersTab.Length; j++)
+                    {
+                        if (numbersTab[j] < numbersTab[minIndex])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                    if (minIndex != i)
+                    {
+                        temp = numbersTab[minIndex];
+                        numbersTab[minIndex] = numbersTab[i];
+                        numbersTab[i] = temp;
+                    }
+                }
+
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                timeValue.Text = timeSpan.TotalSeconds + " s";
+
+                string sortedTab = string.Join(" ", numbersTab);
+                textBox2.Text = sortedTab;
+
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            //SI
+            int[] numbersTab = Convert(textBox1.Text);
+
+            if(numbersTab != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int temp = 0;
+
+                for(int i=1; i<numbersTab.Length; i++)
+                {
+                    temp = numbersTab[i];
+                    int j = i - 1;
+                    while(j >= 0 && numbersTab[j] > temp)
+                    {
+                        numbersTab[j + 1] = numbersTab[j];
+                        j -= 1; // jesli j mniejsze od 0 to nie sprawdzamy juz po lewej j
+                    }
+                    numbersTab[j + 1] = temp;
+                }
+
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                timeValue.Text = timeSpan.TotalSeconds + " s";
+
+                string sortedTab = string.Join(" ", numbersTab);
+                textBox2.Text = sortedTab;
+            }
+        }
+
+        private void SM_Click(object sender, EventArgs e)
+        {
+            //SM  
+            int[] numbersTab = Convert(textBox1.Text);
+
+            if(numbersTab != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                mergeSort(numbersTab);
+
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                timeValue.Text = timeSpan.TotalSeconds + " s";
+
+                string sortedTab = string.Join(" ", numbersTab);
+                textBox2.Text = sortedTab;
+            }
+        }
+
+        private void mergeSort(int[] numbersTab)
+        {
+            if(numbersTab.Length <= 1)
+            {
+                return;
+            }
+
+            int mid = numbersTab.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[numbersTab.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+            {
+                left[i] = numbersTab[i];
+            }
+            for (int i = mid; i < numbersTab.Length; i++)
+            {
+                right[i-mid] = numbersTab[i];
+            }
+
+            mergeSort(left);
+            mergeSort(right);
+
+            // Kiedy mamy juz pojedyncze elementy jako tablicy laczymy je w wieksze tablice
+            int leftIndex = 0, rightIndex = 0, index = 0;
+            while(leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    numbersTab[index] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    numbersTab[index] = right[rightIndex];
+                    rightIndex++;
+                }
+                index++;
+            }
+
+            while(leftIndex < left.Length)
+            {
+                numbersTab[index] = left[leftIndex];
+                leftIndex++;
+                index++;
+            }
+
+            while(rightIndex < right.Length)
+            {
+                numbersTab[index] = right[rightIndex];
+                rightIndex++;
+                index++;
+            }
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            //SQ
+            
+            int[] numbersTab = Convert(textBox1.Text);
+            if(numbersTab != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                quickSort(numbersTab, 0, numbersTab.Length - 1);
+
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                timeValue.Text = timeSpan.TotalSeconds + " s";
+
+                string sortedTab = string.Join(" ", numbersTab);
+                textBox2.Text = sortedTab;
+            }
+        }
+
+        private void quickSort(int[] numbersTab, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotTemp = pivotIndex(numbersTab, left, right);
+                quickSort(numbersTab, left, pivotTemp-1);
+                quickSort(numbersTab, pivotTemp+1, right);
+            }
+        }
+
+        private int pivotIndex(int[] numbersTab, int left, int right)
+        {
+            int pivot = numbersTab[right];
+            int j = left - 1;
+            for(int i=left; i<right; i++)
+            {
+                if (numbersTab[i] <= pivot)
+                {
+                    j++;
+                    int temp = numbersTab[j];
+                    numbersTab[j] = numbersTab[i];
+                    numbersTab[i] = temp;
+                }
+            }
+            int tempPivot = numbersTab[j + 1];
+            numbersTab[j + 1] = numbersTab[right];
+            numbersTab[right] = tempPivot;
+
+            return j + 1;
+        }
+
+        private void timeValue_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Generuj_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int size = (int)numericUpDown1.Value;
+            int[] numbersTab = new int[size];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < numbersTab.Length; i++)
+            {
+                numbersTab[i] = random.Next(-2000, 2000);
+            }
+            textBox1.Text = string.Join(" ", numbersTab);
+        }
+
+    }
+}
