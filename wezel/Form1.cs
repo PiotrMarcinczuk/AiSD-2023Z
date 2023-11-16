9e1d20057cef3b5bd278377ce22e6029310ed7fe
Oskar Skowroński <100282489+PiotrMarcinczuk@users.noreply.github.com>
2023-11-16 18:10:34 +0100

    przechodzenie

--- wezel/Form1.cs
+++ wezel/Form1.cs
@@ -42,12 +42,63 @@ namespace wezel
                 MessageBox.Show(dziecko.ToString());
                 A(dziecko);
             }
-            
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var w1 = new Wezel2(1);
+            var w2 = new Wezel2(2);
+            var w3 = new Wezel2(3);
+            var w4 = new Wezel2(4);
+            var w5 = new Wezel2(5);
+            var w6 = new Wezel2(6);
+            var w7 = new Wezel2(7);
+
+            //w1.neighbours.Add(w2);
+            //w2.neighbours.Add(w1);
+            w1.Add(w2);
+            //w1.neighbours.Add(w3);
+            //w3.neighbours.Add(w1);
+            w1.Add(w3);
+            //w2.neighbours.Add(w4);
+            //w4.neighbours.Add(w2);
+            //w2.neighbours.Add(w5);
+            //w5.neighbours.Add(w2);
+            w2.Add(w4);
+            w2.Add(w5);
+            //w3.neighbours.Add(w6);
+            //w6.neighbours.Add(w3);
+            //w3.neighbours.Add(w7);
+            //w7.neighbours.Add(w3);
+            w3.Add(w6);
+            w3.Add(w7);
+            //w7.neighbours.Add(w4);
+            //w4.neighbours.Add(w7);
+            w7.Add(w4);
+            A(w1);
+        }
+
+        List<Wezel2> odwiedzone = new List<Wezel2>();
+        public void A(Wezel2 w)
+        {
+            
+            odwiedzone.Clear();
+
+            foreach (var neighbour in w.neighbours)
+            {
+                MessageBox.Show(neighbour.ToString());
+                if (odwiedzone.Contains(neighbour))
+                {
+                    odwiedzone.Add(neighbour);
+                    A(neighbour);
+                }
+
+            }
         }
     }
 
@@ -65,6 +116,30 @@ namespace wezel
         {
             return "Wartość: " + this.wartosc.ToString();
         }
+    }
+
+    public class Wezel2
+    {
+        public int wartosc;
+        public List<Wezel2> neighbours = new List<Wezel2>();
+
+        public Wezel2(int wartosc)
+        {
+            this.wartosc = wartosc;
+        }
+
+        public void Add(Wezel2 w)
+        {
+            this.neighbours.Add(w);
+            w.neighbours.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return "Wartość: " + this.wartosc.ToString();
+        }
+
+        
     }
         
 }
